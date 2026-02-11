14c214eb625ec40107c5817431de9a2185618073
indikbal <indikbal@gmail.com>
2026-02-11 09:34:00 +0530

    delete karan

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -4,7 +4,6 @@ import team2Image from '../img/team2.jpg';
 import team3Image from '../img/team3.jpg';
 import team8Image from '../img/team8.jpg';
 import team10Image from '../img/team10.jpg';
-import team11Image from '../img/team11.jpg';
 
 
 
@@ -53,12 +52,7 @@ const teamMembers = [
     name: 'Monika',
     role: 'Innovation Specialist',
     image: team10Image,
-  },
-  {
-    name: 'Karan',
-    role: 'Ops Manager',
-    image: team11Image,
-  },
+  }
   
 ];
 
